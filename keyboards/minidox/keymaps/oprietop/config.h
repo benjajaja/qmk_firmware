8f436d08de47aeff9961c5e1f3f70dbe96a20126
oprietop <oprietop@uoc.edu>
2018-08-31 14:25:00 -0700

    working minidox

--- keyboards/minidox/keymaps/oprietop/config.h
+++ keyboards/minidox/keymaps/oprietop/config.h
@@ -1,18 +1,18 @@
-/* Copyright 2017 Benjamin Kesselring
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*
+This is the c configuration file for the keymap
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
@@ -23,20 +23,16 @@
 
 #define EE_HANDS
 
-/* ws2812 RGB LED */
-#define RGB_DI_PIN D7
-#define RGBLIGHT_TIMER
-#define RGBLED_NUM 10   // Number of LEDs
-#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
-
 /* Mouse Settings */
 #define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
 #define MOUSEKEY_TIME_TO_MAX 60
 #define MOUSEKEY_MAX_SPEED 7
 #define MOUSEKEY_WHEEL_DELAY 0
+
+/* RGB LED */
+#define RGB_DI_PIN B3
+#define RGBLED_NUM 10   // Number of LEDs
+#define RGBLIGHT_ANIMATIONS
 
 #endif
