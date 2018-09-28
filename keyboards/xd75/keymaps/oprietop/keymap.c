abe339f4db1caf244e553b950930477c488ced4d
Oscar Prieto <oprieto@externos.itnow.es>
2018-09-28 16:01:35 +0200

    toying with the minidox

--- keyboards/xd75/keymaps/oprietop/keymap.c
+++ keyboards/xd75/keymaps/oprietop/keymap.c
@@ -1,18 +1,3 @@
-/* Copyright 2017 Wunder
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include "xd75.h"
 
 // Layer shorthand
@@ -23,8 +8,14 @@
 // Shift when held, Enter when tapped
 #define KC_LSEN LSFT_T(KC_ENT)
 #define KC_RSEN RSFT_T(KC_ENT)
+
 // Shift when held, Space when tapped
 #define KC_LSSP LSFT_T(KC_SPC)
+
+// Cut/Paste shortcuts
+#define PASTE RSFT(KC_INS)
+#define CUT LSFT(KC_DEL)
+#define CPY LCTL(KC_INS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -61,20 +52,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | RESET  | F1     | F2     | F3     | F4     | F5     | F11    | F12    | F6     | F7     | F8     | F9     | F10    |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | WWW    | MYCOMP | CALC   | MAIL   |        | RGB HD | RGB HI | MOD-DH | P7     | P8     | P9     | M BTN3 | MW UP  | M BTN4 |
+ * |        | WWW    | MYCOMP | CALC   | MAIL   | RGB_TOG| RGB HD | RGB HI | MOD-DH | P7     | P8     | P9     | M BTN3 | MW UP  | M BTN4 |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | CAPSLK | PLAY   | PREV   | NEXT   | STOP   |        | RGB SD | RGB SI | QWERTY | P4     | P5     | P6     | MWLEFT | MWDOWM | MWRGTH |
+ * | CAPSLK | PLAY   | PREV   | NEXT   | STOP   | RGB_MOD| RGB SD | RGB SI | QWERTY | P4     | P5     | P6     | MWLEFT | MWDOWM | MWRGTH |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | MUTE   | VOL-   | VOL+   | MEDIA  |        | RGB VD | RGB VI |        | P1     | P2     | P3     | M BTN1 | M UP   | M BTN2 |
+ * |        | MUTE   | VOL-   | VOL+   | MEDIA  | PASTE  | RGB VD | RGB VI | CUT    | P1     | P2     | P3     | M BTN1 | M UP   | M BTN2 |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        | RGB TG | RGBMOD | RGBRMO | RGB TG |        | P0     | P.     | M LEFT | M DOWN | M RGHT |
+ * |        |        |        |        |        |        |        |        |        |        | P0     | P.     | M LEFT | M DOWN | M RGHT |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
  [_FN] = { /* FUNCTION */
   { RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,  KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______  },
   { _______, KC_WSCH, KC_MYCM, KC_CALC, KC_MAIL, RGB_TOG, RGB_HUD, RGB_HUI, TG(_CO), KC_P7,   KC_P8,   KC_P9,   KC_BTN3, KC_WH_U, KC_BTN4  },
   { KC_CAPS, KC_MPLY, KC_MPRV, KC_MNXT, KC_MSTP, RGB_MOD, RGB_SAD, RGB_SAI, TG(_QW), KC_P4,   KC_P5,   KC_P6,   KC_WH_L, KC_WH_D, KC_WH_R  },
-  { _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MSEL, _______, RGB_VAD, RGB_VAI, _______, KC_P1,   KC_P2,   KC_P3,   KC_BTN1, KC_MS_U, KC_BTN2  },
+  { _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MSEL, PASTE,   RGB_VAD, RGB_VAI, CUT,     KC_P1,   KC_P2,   KC_P3,   KC_BTN1, KC_MS_U, KC_BTN2  },
   { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_0,    KC_PDOT, KC_MS_L, KC_MS_D, KC_MS_R  },
  }
 
@@ -82,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Runs boot tasks for keyboard
 void matrix_init_user(void) {
-        rgblight_enable();
-        rgblight_sethsv(150, 255, 255); // Spring Green
-        rgblight_mode(13); // Swirling rainbow
+  rgblight_enable();
+  rgblight_sethsv(150, 255, 255); // Spring Green
+  rgblight_mode(13); // Swirling rainbow
 };
