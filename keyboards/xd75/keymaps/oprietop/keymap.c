5b1f48f6cf171c96f6e2f6312cfa1ad0fa0a8053
Oscar Prieto <oprieto@externos.itnow.es>
2019-05-10 15:37:19 +0200

    changing the xd75 keymap

--- keyboards/xd75/keymaps/oprietop/keymap.c
+++ keyboards/xd75/keymaps/oprietop/keymap.c
@@ -4,13 +4,12 @@
 #define _CO 0
 #define _QW 1
 #define _FN 2
+#define _AD 3
 // Shift when held, Enter when tapped
 #define LSEN LSFT_T(KC_ENT)
 #define RSEN RSFT_T(KC_ENT)
 // Shift when held, Space when tapped
 #define LSSP LSFT_T(KC_SPC)
-// ALTGR when held, Space when tappec
-#define RALSPC RALT_T(KC_SPC)
 // Cut/Paste shortcuts
 #define PASTE RSFT(KC_INS)
 #define CUT LSFT(KC_DEL)
@@ -35,38 +34,58 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_LBRC, KC_PGDN, KC_RBRC, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS },
     { KC_BSPC, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_DEL,  KC_INS,  KC_GRV,  KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT },
     { LSEN,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_MINS, KC_UP,   KC_EQL,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, RSEN    },
-    { KC_LCTL, KC_LGUI, KC_LALT, KC_RALT, TT(_FN), LSSP,    KC_LEFT, KC_DOWN, KC_RGHT, RSEN,    TT(_FN), KC_CAPS, KC_RALT, KC_RGUI, KC_RCTL },
+    { KC_LCTL, KC_LGUI, KC_LALT, KC_RALT, TT(_FN), LSSP,    KC_LEFT, KC_DOWN, KC_RGHT, RSEN,    TT(_FN), KC_RALT, KC_LALT, KC_RGUI, KC_RCTL },
   },
   [_QW] = { /* QWERTY */
     { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-    { _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______ },
-    { _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______ },
-    { _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______ },
+    { _______, _______, _______, KC_E,    KC_R,    KC_T,    _______, _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______ },
+    { _______, KC_A,    KC_S,    KC_D,    KC_F,    _______, _______, _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______ },
+    { _______, _______, _______, _______, KC_V,    KC_B,    _______, _______, _______, KC_N,    KC_M,    _______, _______, _______, _______ },
     { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
   },
  /* FUNCTION
-  * .--------------------------------------------------------------------------------------------------------------------------------------.
+  * .--------------------------------------------------------------------------------------------------------------------------V------------.
   * | F11    | F1     | F2     | F3     | F4     | F5     | P/     | P*     | P-     | F6     | F7     | F8     | F9     | F10    | F12    |
   * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-  * | PRSCR  | ESC    | CUT    | WH_UP  | PASTE  | VOL +  | P7     | P8     | P9     | NEXT   | MOU_B1 | MOU_UP | MOU_B2 | MOU_B3 | MOU_B4 |
+  * |        | ESC    | CUT    | WH_UP  | PASTE  | VOL +  | P7     | P8     | P9     | PGUP   | MOU_B1 | MOU_UP | MOU_B2 | MOU_B3 |        |
   * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-  * | RGB_MOD| TAB    | WH_LE  | WH_DO  | WH_RG  | VOL -  | P4     | P5     | P6     | PREV   | MOU_LE | MOU_DO | MOU_RG | PGUP   | FN TOG |
+  * |        | TAB    | WH_LE  | WH_DO  | WH_RG  | VOL -  | P4     | P5     | P6     | HOME   | MOU_LE | MOU_DO | MOU_RG | PGUP   |        |
   * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-  * | RGB_TOG| LCTL   | LSFT   | LGUI   | MOU_B1 | MUTE   | P1     | P2     | P3     | PLAY   | HOME   | END    | UP     | PGDOWN |        |
+  * |        | LCTL   | LSFT   | LGUI   | MOU_B1 | MUTE   | P1     | P2     | P3     | PGDN   | LEFT   | DOWN   | UP     | RIGHT  |        |
   * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-  * | RESET  | CO/QW  | SCRLCK | PAUSE  | NUMLCK | SPACE  | P0     | P.     | P+     | PENT   | NUMLCK | LEFT   | DOWN   | RIGHT  |        |
+  * |        |        |        |        | ADJUST | SPACE  | P0     | P.     | P+     | PENT   | ADJUST |        |        |        |        |
   * '--------------------------------------------------------------------------------------------------------------------------------------'
   */
   [_FN] = { /* FUNCTION */
     { KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_PSLS, KC_PAST, KC_PMNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12  },
-    { KC_PSCR, KC_ESC,  CUT,     KC_WH_U, PASTE,   KC_VOLU, KC_P7,   KC_P8,   KC_P9,   KC_MNXT, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, KC_BTN4 },
-    { RGB_MOD, KC_TAB,  KC_WH_L, KC_WH_D, KC_WH_R, KC_VOLD, KC_P4,   KC_P5,   KC_P6,   KC_MPRV, KC_MS_L, KC_MS_D, KC_MS_R, KC_PGUP, TG(_FN) },
-    { RGB_TOG, KC_LCTL, KC_LSFT, KC_LGUI, KC_BTN1, KC_MUTE, KC_P1,   KC_P2,   KC_P3,   KC_MPLY, KC_HOME, KC_END,  KC_UP,   KC_PGDN, _______ },
-    { RESET,   TG(_QW), KC_SLCK, KC_PAUS, KC_NLCK, KC_SPC,  KC_P0,   KC_PDOT, KC_PPLS, KC_PENT, KC_NLCK, KC_LEFT, KC_DOWN, KC_RGHT, _______ },
+    { _______, KC_ESC,  CUT,     KC_WH_U, PASTE,   KC_VOLU, KC_P7,   KC_P8,   KC_P9,   KC_PGUP, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, KC_BTN4 },
+    { _______, KC_TAB,  KC_WH_L, KC_WH_D, KC_WH_R, KC_VOLD, KC_P4,   KC_P5,   KC_P6,   KC_HOME, KC_MS_L, KC_MS_D, KC_MS_R, KC_END,  _______ },
+    { _______, KC_LCTL, KC_LSFT, KC_LGUI, KC_BTN1, KC_MUTE, KC_P1,   KC_P2,   KC_P3,   KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______ },
+    { _______, _______, _______, _______, TT(_AD), KC_SPC,  KC_P0,   KC_PDOT, KC_PPLS, TT(_AD), _______, _______, _______, _______, _______ },
+  },
+ /* ADJUST
+  * .--------------------------------------------------------------------------------------------------------------------------------------.
+  * | F11    | F1     | F2     | F3     | F4     | F5     | P/     | P*     | P-     | F6     | F7     | F8     | F9     | F10    | F12    |
+  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+  * |        | ESC    | CUT    | WH_UP  | PASTE  | VOL +  | P7     | P8     | P9     | PGUP   | MOU_B1 | MOU_UP | MOU_B2 | MOU_B3 | MOU_B4 |
+  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+  * | RGB_MOD| TAB    | WH_LE  | WH_DO  | WH_RG  | VOL -  | P4     | P5     | P6     | HOME   | MOU_LE | MOU_DO | MOU_RG | PGUP   | FN TOG |
+  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+  * | RGB_TOG| LCTL   | LSFT   | LGUI   | MOU_B1 | MUTE   | P1     | P2     | P3     | PGDN   | LEFT   | DOWN   | UP     | RIGHT  |        |
+  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+  * | RESET  | CO/QW  | SCRLCK | PAUSE  | NUMLCK | SPACE  | P0     | P.     | P+     | PENT   | NUMLCK | LEFT   | DOWN   | RIGHT  |        |
+  * '--------------------------------------------------------------------------------------------------------------------------------------'
+  */
+  [_AD] = { /* ADJUST */
+    { RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_PSLS, KC_PAST, KC_PMNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12  },
+    { _______, KC_ESC,  CUT,     KC_WH_U, PASTE,   KC_VOLU, KC_P7,   KC_P8,   KC_P9,   KC_PGUP, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, _______ },
+    { _______, KC_TAB,  KC_WH_L, KC_WH_D, KC_WH_R, KC_VOLD, KC_P4,   KC_P5,   KC_P6,   KC_HOME, KC_MS_L, KC_MS_D, KC_MS_R, KC_END,  _______ },
+    { _______, KC_LCTL, KC_LSFT, KC_LGUI, KC_BTN1, KC_MUTE, KC_P1,   KC_P2,   KC_P3,   KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______ },
+    { _______, TG(_QW), KC_SLCK, KC_PAUS, KC_NLCK, KC_SPC,  KC_P0,   KC_PDOT, KC_PPLS, KC_PENT, KC_NLCK, KC_LEFT, KC_DOWN, KC_RGHT, _______ },
   }
 };
 
-// Runs boot tasks for keyboard22222
+// Runs boot tasks for keyboard
 void matrix_init_user(void) {
   rgblight_enable();
   //rgblight_sethsv(169, 255, 255); // Pulse CYAN
