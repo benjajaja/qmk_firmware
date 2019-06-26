eebea6b017302223ee3f7cc4448015312960fefe
Oscar Prieto <oprieto@externos.itnow.es>
2019-06-26 10:35:28 +0200

    ironing the abd_usb keymap

--- keyboards/converter/adb_usb/keymaps/oprietop/keymap.c
+++ keyboards/converter/adb_usb/keymaps/oprietop/keymap.c
@@ -1,78 +1,81 @@
 #include QMK_KEYBOARD_H
 
-// Timer for M_BSPC
-static uint16_t timer;
+// Allow printing version info
+#include "version.h"
 
 // Layers
 #define _QW 0
 #define _DH 1
-#define _LO 2
-#define _RA 3
-#define _AD 4
+#define _FN 2
 
 enum custom_keycodes {
   M_BSPC = SAFE_RANGE,
   M_WIPE,
   M_RAN64,
-  UC_FLIP,
-  UC_TABL,
-  UC_SHRG,
-  UC_DISA,
-  UC_DANC,
-  UC_SCRE,
-  UC_WALL,
-  UC_SOB,
-  UC_DEAL,
-  UC_RAGE,
-  UC_NOOO,
-  UC_SALU,
+  M_VER,
+  M_PULL,
+  M_PUSH,
+  M_LS,
+  M_INCL,
+  #ifdef UNICODE_ENABLE
+    UC_FLIP,
+    UC_TABL,
+    UC_SHRG,
+    UC_DISA,
+    UC_DANC,
+    UC_SCRE,
+    UC_WALL,
+    UC_SOB,
+    UC_DEAL,
+    UC_RAGE,
+    UC_NOOO,
+    UC_SALU,
+  #endif
 };
 
 // Shortcuts
 #define COPY LCTL(KC_INS)
 #define CUT LSFT(KC_DEL)
 #define PASTE RSFT(KC_INS)
-#define KC_BL_S BL_STEP
-#define KC_BL_B BL_BRTG
-
-// Mod-Taps
-// https://github.com/qmk/qmk_firmware/blob/master/docs/feature_advanced_keycodes.md
-#define LST(X) LSFT_T(X)
-#define RST(X) RSFT_T(X)
-#define LCT(X) LCTL_T(X)
-#define RCT(X) RCTL_T(X)
-#define LGT(X) LGUI_T(X)
-#define RGT(X) RGUI_T(X)
-#define TAT(X) LALT_T(X)
-#define AGT(X) RALT_T(X)
-
-// Modifiers
-#define AD_ESC LT(_AD, KC_ESC)
-#define LT_GUI LT(_AD, KC_RGUI)
-#define LT_TAB LT(_LO, KC_TAB)
-#define LO_SPC LT(_LO, KC_SPC)
-#define S_SPC LSFT_T(KC_SPC)
-#define S_TAB LSFT_T(KC_TAB)
-#define S_DEL RSFT_T(KC_DEL)
-#define RA_BSPC LT(_RA, KC_BSPC)
+#define FN_SPC LT(_FN, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT_ext_iso(
-      KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,          KC_PSCR, KC_SLCK, KC_PAUS,                              DF(_DH),
-      KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_INS , KC_HOME, KC_PGUP,   KC_NLCK, KC_PEQL, KC_PSLS, KC_PAST,
-      KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_ENT , KC_DEL , KC_END , KC_PGDN,   KC_P7  , KC_P8  , KC_P9  , KC_PMNS,
-      KC_LCAP, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS,                                       KC_P4  , KC_P5  , KC_P6  , KC_PPLS,
-      KC_LSFT, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,                   KC_UP  ,            KC_P1  , KC_P2  , KC_P3  ,
-      KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC ,                                     KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT,            KC_P0  , KC_PDOT, KC_PENT
+    KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,          KC_PSCR, KC_SLCK, KC_PAUS,                              MO(_FN),
+    KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_INS , KC_HOME, KC_PGUP,   KC_NLCK, KC_PEQL, KC_PSLS, KC_PAST,
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_ENT , KC_DEL , KC_END , KC_PGDN,   KC_P7  , KC_P8  , KC_P9  , KC_PMNS,
+    KC_LCAP, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS,                                       KC_P4  , KC_P5  , KC_P6  , KC_PPLS,
+    KC_LSFT, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,                   KC_UP  ,            KC_P1  , KC_P2  , KC_P3  ,
+    KC_LCTL, KC_LALT, KC_LGUI,                            FN_SPC ,                                     KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT,            KC_P0  , KC_PDOT, KC_PENT
   ),
+
   [_DH] = LAYOUT_ext_iso(
-      KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,          KC_PSCR, KC_SLCK, KC_PAUS,                              DF(_QW),
-      KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_INS , KC_HOME, KC_PGUP,   KC_NLCK, KC_PEQL, KC_PSLS, KC_PAST,
-      KC_TAB , KC_Q   , KC_W   , KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_LBRC, KC_RBRC, KC_ENT , KC_DEL , KC_END , KC_PGDN,   KC_P7  , KC_P8  , KC_P9  , KC_PMNS,
-      KC_LCAP, KC_A   , KC_R   , KC_S   , KC_T   , KC_G   , KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , KC_QUOT, KC_NUHS,                                       KC_P4  , KC_P5  , KC_P6  , KC_PPLS,
-      KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_NUBS, KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,                   KC_UP  ,            KC_P1  , KC_P2  , KC_P3  ,
-      KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC ,                                     KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT,            KC_P0  , KC_PDOT, KC_PENT
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,                              _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______,
+    _______, _______, _______, KC_F   , KC_P   , KC_B   , KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______,
+    _______, _______, KC_R   , KC_S   , KC_T   , _______, KC_M   , KC_N   , KC_E   , KC_I   , KC_O   , _______, _______,                                       _______, _______, _______, _______,
+    _______, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   , KC_NUBS, KC_K   , KC_H   , _______, _______, _______, _______,                   _______,            _______, _______, _______,
+    _______, _______, _______,                            _______,                                     _______, _______,          _______, _______, _______,            _______, _______, _______
   ),
+
+  [_FN] = LAYOUT_ext_iso(
+    RESET  , UC_FLIP, UC_TABL, UC_SHRG, UC_DISA, UC_DANC, UC_SCRE, UC_WALL, UC_SOB , UC_DEAL, UC_RAGE, UC_NOOO, UC_SALU,          DF(_QW), DF(_DH), M_VER  ,                              XXXXXXX,
+    M_VER  , M_LS   , M_PULL , M_PUSH , M_INCL , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______,
+    _______, KC_VOLU, KC_ESC , COPY   , KC_WH_U, CUT    , PASTE  , KC_PGUP, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, KC_MNXT, _______, _______, _______, _______,   _______, _______, _______, _______,
+    _______, KC_VOLD, KC_TAB,  KC_WH_L, KC_WH_D, KC_WH_R, KC_BTN3, KC_HOME, KC_MS_L, KC_MS_D, KC_MS_R, KC_END,  KC_MPRV,                                       _______, _______, _______, _______,
+    _______, KC_MUTE, KC_LCTL, KC_LSFT, KC_LGUI, KC_BTN1, KC_BTN2, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MPLY,                   _______,            _______, _______, _______,
+    _______, _______, _______,                            M_RAN64,                                     _______, _______,          _______, _______, _______,            _______, _______, _______
+  ),
+
+/*  [_EM] = LAYOUT_ext_iso(
+ *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,                              _______,
+ *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______,
+ *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______,
+ *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                                       _______, _______, _______, _______,
+ *   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,            _______, _______, _______,
+ *   _______, _______, _______,                            _______,                                     _______, _______,          _______, _______, _______,            _______, _______, _______
+ * ),
+ */
 };
 
 void matrix_init_user(void) { // Runs boot tasks for keyboard
@@ -81,47 +84,8 @@ void matrix_init_user(void) { // Runs boot tasks for keyboard
   #endif
 };
 
-static uint16_t bspc_role;
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  #ifdef OLED_DRIVER_ENABL
-    if (record->event.pressed) {  add_keylog(keycode); }
-  #endif
   switch (keycode) {
-    // Our funky backspace key!
-    case M_BSPC:
-      if (get_mods() & MOD_BIT(KC_LSHIFT)) {
-        bspc_role = KC_ENT;
-      } else {
-        bspc_role = KC_BSPC;
-      }
-      if (record->event.pressed) {
-        if (timer_elapsed(timer) > TAPPING_TERM) {
-          register_code(KC_RSFT);
-        } else {
-          register_code(bspc_role);
-        }
-        timer = timer_read();
-      } else {
-        // Unregister Right Shift if registered
-        if (get_mods() & MOD_BIT(KC_RSHIFT)) {
-          unregister_code(KC_RSFT);
-        }
-        // Check if we are into the TAPPING_TERM threshold
-        if (timer_elapsed(timer) < TAPPING_TERM) {
-          switch (bspc_role) {
-            case KC_ENT:
-              unregister_code(KC_LSFT);
-              register_code(bspc_role);
-              register_code(KC_LSFT);
-            case KC_BSPC:
-              register_code(bspc_role);
-          }
-        }
-        unregister_code(bspc_role);
-      }
-      return false;
-      break;
     case M_WIPE: // Wipe the eeprom and reset the board
       if (record->event.pressed) {
         eeconfig_init();
@@ -135,6 +99,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case M_INCL:
+      if (record->event.pressed) {
+        SEND_STRING("#include <>"SS_TAP(X_LEFT));
+      }
+      return false;
+      break;
+      return false;
+    case M_PULL:
+      if (record->event.pressed) {
+        SEND_STRING("git pull"SS_TAP(X_ENTER));
+      }
+      return false;
+      break;
+      return false;
+    case M_PUSH:
+      if (record->event.pressed) {
+        SEND_STRING("git push"SS_TAP(X_ENTER));
+      }
+      return false;
+      break;
+      return false;
+    case M_LS:
+      if (record->event.pressed) {
+        SEND_STRING("ls -la"SS_TAP(X_ENTER));
+      }
+      return false;
+      break;
+      return false;
+    case M_VER:
+      if (record->event.pressed) {
+        SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP "@" QMK_VERSION "@" QMK_BUILDDATE);
+      }
+      return false;
+      break;
+      return false;
     #ifdef UNICODE_ENABLE
       case UC_FLIP: // (ノಠ痊ಠ)ノ彡┻━┻
         if (record->event.pressed) {
