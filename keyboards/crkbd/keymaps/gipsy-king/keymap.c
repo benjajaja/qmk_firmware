23312ca9ef6e0bfbc837074f799f97353f426c73
Benjamin Große <benjamin@midokura.com>
2020-08-25 09:50:46 +0200

    WIP alternate toggle adjust layer from master

--- keyboards/crkbd/keymaps/gipsy-king/keymap.c
+++ keyboards/crkbd/keymaps/gipsy-king/keymap.c
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,      KC_Q,     KC_W,     KC_E, KC_R, KC_T,            KC_Y, KC_U, KC_I,    KC_O,        KC_P,         KC_BSPC, \
       KC_ESC,      KC_A,     KC_S,     KC_D, KC_F, KC_G,            KC_H, KC_J, KC_K,    KC_L,        KC_SCLN,      KC_QUOT, \
       LCT(KC_MINS),TAT(KC_Z),LGT(KC_X),KC_C, KC_V, KC_B,            KC_N, KC_M, KC_COMM, RGT(KC_DOT), AGT(KC_SLSH), RCT(KC_EQL),\
-      MOD4, KC_LSPO, LT(_RA, KC_SPC),            LT(_RA, KC_ENT), KC_RSPC, MO(_AD) \
+                         MOD4, KC_LSPO, LT(_RA, KC_SPC),            LT(_RA, KC_ENT), KC_RSPC, MO(_AD) \
       ),
   /* Raise
    * ,-----------------------------------------.         ,-----------------------------------------.
@@ -128,14 +128,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_P,    XXXXXXX, \
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_T,        KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX, XXXXXXX, \
       XXXXXXX, XXXXXXX, XXXXXXX, KC_C,    XXXXXXX, KC_B,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-      KC_LSFT, XXXXXXX, KC_SPC,     KC_ENT,  XXXXXXX, XXXXXXX \
+      KC_LSFT, TO(_AD), KC_SPC,     KC_ENT,  XXXXXXX, XXXXXXX \
       ),
   // F1-12, RGB, Gimmicks
   [_AD] = LAYOUT (
       M_WIPE, RGB_RMOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_BRIU,           UC_FLIP, UC_DISA, UC_SHRG, UC_CRY,  XXXXXXX, XXXXXXX, \
-      RGB_TOG, RGB_MOD,  RGB_HUD, RGB_SAD, RGB_VAD, KC_BRID,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
-      KC_F11,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
-      XXXXXXX, M_RAN64, XXXXXXX,           KC_BSPC, _______, XXXXXXX \
+      RGB_TOG, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_BRID,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, \
+      KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,             KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,  \
+      M_RAN64, TG(_AD), XXXXXXX,           KC_BSPC, _______, XXXXXXX \
       ),
 };
 
