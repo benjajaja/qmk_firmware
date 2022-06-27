54ac4775476d4f8865edc7846bf77a4a232897c7
Benjamin Große <ste3ls@gmail.com>
2022-06-27 09:01:48 +0100

    missing xmonad stuff, email, tabs

--- keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
+++ keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
@@ -23,6 +23,8 @@
 #define ONESHOT OSL(3)
 // Mic-mute-tap or Fn layer (F20 is mic-mute on thinkpads)
 #define FNLAY LT(3, KC_F20)
+// Ctrl+Shift Shortcuts that are awkward without this layer
+#define CTRL LM(4, MOD_LCTL)
 
 enum custom_keycodes {
     EMAIL = SAFE_RANGE,
@@ -45,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
   KC_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
   KC_MINS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_EQL,
-  KC_LCTL,KC_LALT,KC_LGUI,MOD4,   KC_LSPO,KC_SPC, RAISE,  KC_RSPC,KC_RALT,KC_RGUI,FNLAY,  KC_RCTL
+  CTRL,   KC_LALT,KC_LGUI,MOD4,   KC_LSPO,KC_SPC, RAISE,  KC_RSPC,KC_RALT,KC_RGUI,FNLAY,  KC_RCTL
 ),
 /* Raise
  * .-----------------------------------------------------------------------------------.
@@ -64,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LBRC,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_RBRC,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,ONESHOT,_______,_______
 ),
-/* XMONAD 
+/* XMONAD
  * Top row: Quit, Workspace shortcuts, Run
  * Middle row: Float, HJKL
  * Bottom row: Shift, Close, Border, Keyboard reset
@@ -72,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [2] = LAYOUT(
   KC_Q,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_P,   KC_MUTE,
-  KC_LSFT,_______,_______,_______,_______,KC_T,   KC_H,   KC_J,   KC_K,   KC_L,   _______,_______,
+  KC_LSFT,_______,KC_W,   KC_E,   KC_R,   KC_T,   KC_H,   KC_J,   KC_K,   KC_L,   _______,_______,
   KC_LSFT,_______,_______,KC_C,   _______,KC_B,   _______,_______,_______,_______,_______,_______,
   _______,_______,_______,KC_LSFT,KC_LSFT,KC_SPC, KC_ENT, KC_LSFT,_______,_______,KC_VOLD,KC_VOLU
 ),
@@ -84,6 +86,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,EMAIL_W,EMAIL,  _______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
   RESET,  _______,_______,_______,_______,_______,KC_BTN1,KC_MUTE,KC_VOLD,KC_VOLU,KC_BRID,KC_BRIU,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
+),
+/* Ctrl Shortcuts
+ * These are awkward without this layer
+ */
+[4] = LAYOUT(
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,KC_PGDN,KC_PGUP,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
 )
 };
 
@@ -94,7 +105,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING("ste3ls@gmail.com");
                 break;
             case EMAIL_W:
-                SEND_STRING("benjamin@midokura.com");
+                SEND_STRING("benjamin.grosse@re-cap.com");
                 break;
         }
     }
