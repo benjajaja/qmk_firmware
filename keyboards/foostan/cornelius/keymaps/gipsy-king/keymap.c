caaa2cff3f29b437eb102af3e62fea304bd1809e
Benjamin Große <ste3ls@gmail.com>
2022-09-28 12:05:08 +0100

    swap -= with []

--- keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
+++ keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Caps |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |   -  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   =  |
+ * |   [  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   ]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | LCtr | LGUI | LAlt | Mod4 | Shift| Space|Ent/Ra| Shift| RALT | RGUI |  FN  |Onesht|
  * '-----------------------------------------------------------------------------------'
@@ -52,24 +52,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
   KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
-  KC_MINS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_EQL,
+  KC_LBRC,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RBRC,
   CTRL,   KC_LGUI,KC_LGUI,MOD4,   KC_LSFT,KC_SPC, RAISE,  KC_RSFT,KC_RALT,KC_RGUI,FNLAY,  ONESHOT
 ),
 /* Raise layer
  * .-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   \  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |   ~  |   {  | PgUp | PgDn |  End | Home | Left | Down |  Up  | Right|   }  |   |  |
+ * |   ~  |   -  | PgUp | PgDn |  End | Home | Left | Down |  Up  | Right|   }  |   |  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |   [  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   ]  |
+ * |   _  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   +  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * '-----------------------------------------------------------------------------------'
  */
 [LAYER_RAISE] = LAYOUT(
   KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,
-  KC_TILD,KC_LCBR,KC_PGUP,KC_PGDN,KC_END, KC_HOME,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_RCBR,KC_PIPE,
-  KC_LBRC,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_RBRC,
+  KC_TILD,KC_MINS,KC_PGUP,KC_PGDN,KC_END, KC_HOME,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_EQL, KC_PIPE,
+  KC_UNDS,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PLUS,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
 ),
 /* Xmonad layer (needs Left-Alt)
