1e7ec0070f48c484351833ef4f18785656955636
Benjamin Große <ste3ls@gmail.com>
2022-09-28 12:20:21 +0100

    reorder some layers/keys

--- keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
+++ keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
@@ -16,13 +16,13 @@
 #include QMK_KEYBOARD_H
 
 #define LAYER_BASE 0
-#define LAYER_RAISE 1
-#define LAYER_XMONAD 2
-#define LAYER_FN 3
+#define LAYER_SYMBOLS 1
+#define LAYER_FN 2
+#define LAYER_XMONAD 3
 #define LAYER_SHORTCUTS 4
 
 // Select raise layer, or enter on tap.
-#define RAISE LT(LAYER_RAISE, KC_ENT)
+#define RAISE LT(LAYER_SYMBOLS, KC_ENT)
 // Select xmonad layer with LALT always on, as that is the xmonad modifier key.
 #define MOD4 LM(LAYER_XMONAD, MOD_LALT)
 // Select fn layer, or mic-mute-tap (F20 is XF86AudioMicMute).
@@ -46,16 +46,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   [  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   ]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | LCtr | LGUI | LAlt | Mod4 | Shift| Space|Ent/Ra| Shift| RALT | RGUI |  FN  |Onesht|
+ * | LCtr | LGUI | LAlt | Mod4 | Shift| Space|Ent/Ra| Shift| RALT |Onesht|  Fn  | LCtrl|
  * '-----------------------------------------------------------------------------------'
  */
 [0] = LAYOUT(
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
   KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
   KC_LBRC,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RBRC,
-  CTRL,   KC_LGUI,KC_LGUI,MOD4,   KC_LSFT,KC_SPC, RAISE,  KC_RSFT,KC_RALT,KC_RGUI,FNLAY,  ONESHOT
+  CTRL,   KC_LGUI,KC_LGUI,MOD4,   KC_LSFT,KC_SPC, RAISE,  KC_RSFT,KC_RALT,ONESHOT,FNLAY,  CTRL
 ),
-/* Raise layer
+/* Symbols layer
  * .-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   \  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -66,10 +66,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * '-----------------------------------------------------------------------------------'
  */
-[LAYER_RAISE] = LAYOUT(
+[LAYER_SYMBOLS] = LAYOUT(
   KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_BSLS,
   KC_TILD,KC_MINS,KC_PGUP,KC_PGDN,KC_END, KC_HOME,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_EQL, KC_PIPE,
   KC_UNDS,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PLUS,
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
+),
+/* Fn layer
+ * FN keys
+ * Email Macros, Keyboard Reset, HJKL as Mouse-move-keys
+ * Mouse button 1, Mute, Volume, Brightness
+ */
+[LAYER_FN] = LAYOUT(
+  KC_F12, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
+  _______,EMAIL_W,EMAIL,  _______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
+  RESET,  _______,_______,_______,_______,_______,KC_BTN1,KC_MUTE,KC_VOLD,KC_VOLU,KC_BRID,KC_BRIU,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
 ),
 /* Xmonad layer (needs Left-Alt)
@@ -92,17 +103,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,_______,KC_W,   KC_E,   KC_R,   KC_T,   KC_H,   KC_J,   KC_K,   KC_L,   _______,_______,
   KC_LSFT,_______,_______,KC_C,   _______,KC_B,   _______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,KC_LSFT,KC_SPC, KC_ENT, KC_LSFT,_______,_______,_______,_______
-),
-/* Fn layer
- * FN keys
- * Email Macros, Keyboard Reset, HJKL as Mouse-move-keys
- * Mouse button 1, Mute, Volume, Brightness
- */
-[LAYER_FN] = LAYOUT(
-  KC_F12, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
-  _______,EMAIL_W,EMAIL,  _______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
-  RESET,  _______,_______,_______,_______,_______,KC_BTN1,KC_MUTE,KC_VOLD,KC_VOLU,KC_BRID,KC_BRIU,
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
 ),
 /* Shortcuts layer (needs Ctrl)
  * This is crutch layer, because getting ctrl + pgup/down is very awkward, having to use too many
