d6c7ecec082f28d644711a05d8e10cef5d6c0615
Benjamin Große <ste3ls@gmail.com>
2023-04-04 08:20:30 +0100

    adjust layout, declare rtc_debug

--- keyboards/crkbd/keymaps/rtc-clockface/keymap.c
+++ keyboards/crkbd/keymaps/rtc-clockface/keymap.c
@@ -36,7 +36,7 @@ enum custom_keycodes {
 #define AGT(X) RALT_T(X)
 
 // Modifiers
-#define MOD4 LM(_LOWER, MOD_LGUI)
+#define MOD4 LM(_LOWER, MOD_LALT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_BASE] = LAYOUT (
       KC_TAB,      KC_Q,     KC_W,     KC_E, KC_R, KC_T,    KC_Y, KC_U, KC_I,    KC_O,        KC_P,         KC_BSPC, \
-      KC_ESC,      KC_A,     KC_S,     KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L,        KC_SCLN,      KC_QUOT, \
+      KC_CAPS,      KC_A,     KC_S,     KC_D, KC_F, KC_G,    KC_H, KC_J, KC_K,    KC_L,        KC_SCLN,      KC_QUOT, \
       LCT(KC_MINS),TAT(KC_Z),LGT(KC_X),KC_C, KC_V, KC_B,    KC_N, KC_M, KC_COMM, RGT(KC_DOT), AGT(KC_SLSH), RCT(KC_EQL),\
                                   MOD4, KC_LSPO, KC_SPC,    LT(_RAISE, KC_ENT), KC_RSPC, KC_MUTE \
       ),
@@ -118,6 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 static uint16_t clock_timeout;
 static bool clock_timeout_off = false;
 static uint16_t clock_skip_timeout;
+static uint8_t rtc_debug[6];
 void matrix_init_user(void) { // Runs boot tasks for keyboard
   clock_timeout = timer_read();
   clock_skip_timeout = timer_read();
