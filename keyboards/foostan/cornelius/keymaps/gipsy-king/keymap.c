31c2aa92f6e8a3316bb1324edbaeda03947b9faf
Benjamin Große <ste3ls@gmail.com>
2024-11-12 09:40:34 +0000

    latest

--- keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
+++ keyboards/foostan/cornelius/keymaps/gipsy-king/keymap.c
@@ -31,6 +31,7 @@
 #define ONESHOT OSL(LAYER_FN)
 // Select shortcuts layer with LCTRL always on.
 #define CTRL LM(LAYER_SHORTCUTS, MOD_LCTL)
+#define QK_BOOT QK_BOOTLOADER
 
 enum custom_keycodes {
     EMAIL = SAFE_RANGE,
@@ -39,23 +40,23 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty base layer
- * Caps should be swapped with Esc on all keyboards by the OS.
- * Left-GUI should be swapped with Left-Alt on all keyboards by the OS.
+ * Caps² should be swapped with Esc on all keyboards by the OS.
+ * Left-GUI³ should be swapped with Left-Alt³ on all keyboards by the OS.
  * .-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |Backsp|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Caps |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |
+ * | Caps²|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   -  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |   =  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI |  Alt | Mod4 | Shift| Space|Ent/Ra| Shift|  Alt |Onesht|  Fn  | Ctrl |
+ * | Ctrl | GUI³ | Alt³ | Mod4 | Shift| Space|Ent/Ra| Shift|  Alt |Onesht|  Fn  | Ctrl |
  * '-----------------------------------------------------------------------------------'
  */
 [0] = LAYOUT(
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
   KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,
   KC_MINS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_EQL,
-  KC_LCTL,KC_LGUI,KC_LGUI,MOD4,   KC_LSFT,KC_SPC, RAISE,  KC_RSFT,KC_RALT,ONESHOT,FNLAY,  KC_RCTL
+  KC_LCTL,KC_LGUI,KC_LALT,MOD4,   KC_LSFT,KC_SPC, RAISE,  KC_RSFT,KC_RALT,ONESHOT,FNLAY,  KC_RCTL
 ),
 /* Symbols layer
  * .-----------------------------------------------------------------------------------.
@@ -81,8 +82,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [LAYER_FN] = LAYOUT(
   KC_F12, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
-  _______,EMAIL_W,EMAIL,  _______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
-  RESET,  _______,_______,_______,_______,_______,KC_BTN1,KC_MUTE,KC_VOLD,KC_VOLU,KC_BRID,KC_BRIU,
+  KC_ESC, EMAIL_W,EMAIL,  _______,_______,_______,KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
+  QK_BOOT,_______,_______,_______,_______,_______,KC_BTN1,KC_MUTE,KC_VOLD,KC_VOLU,KC_BRID,KC_BRIU,
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
 ),
 /* Xmonad layer (needs Left-Alt)
